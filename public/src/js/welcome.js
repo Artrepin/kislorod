1fbbb9eef9e4319318e9149eb45b1b5e83d8ada8
ilya <ilya.soloveyv@gmail.com>
2019-02-06 11:25:03 +0300

    chanch video

--- public/src/js/welcome.js
+++ public/src/js/welcome.js
@@ -2,7 +2,8 @@ $(document).on("click", ".play-box__thumb", function(){
     var c = $('<div class="box-modal box-modal-index-video" />');
         c.prepend(
             '<div class="arcticmodal-close"></div>'+
-            '<video src="/images/video.mp4" controls="controls" autoplay width="100%" poster="/images/main/video-thumb1.jpg">'
+            '<iframe src="https://www.youtube.com/embed/AGIZEZJ8NqE?autoplay=1&amp;loop=1&amp;rel=0&amp;playlist=AGIZEZJ8NqE" allow="autoplay; encrypted-media;" allowfullscreen="" frameborder="0" width="100%" height="100%"></iframe>'
+            // '<video src="/images/video.mp4" controls="controls" autoplay width="100%" poster="/images/main/video-thumb1.jpg">'
         )
         $.arcticmodal({
             content: c,
