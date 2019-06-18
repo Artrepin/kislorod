1076e7adf2607237511fac5bdac0f8d8f8f49b04
web-cubr <mishaoboronov@yandex.ru>
2019-06-18 14:12:22 +0300

    fix

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ const Apartament = require('./models').apartament
 const People = require('./models').people
 const Department = require('./models').department
 
-var Recaptcha = require('express-recaptcha').Recaptcha;
+var Recaptcha = require('express-recaptcha').RecaptchaV3;
 var options = {
     theme: 'light',
     callback: 'RecaptchaSuccess',
